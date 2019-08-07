56af375edb28594f40ec76a53793645fc506e138
David Pechacek <Pechacek_David@bah.com>
2019-08-06 22:03:13 -0400

    Split resapi into two micro-services

---
+++ course-03/exercises/udacity-c3-feedapi/src/config/config.ts
@@ -0,0 +1,23 @@
+export const config = {
+  'dev': {
+    'username': process.env.AWS_DB_USER,
+    'password': process.env.AWS_DB_PASSWORD,
+    'database': process.env.AWS_DB_DATABASE,
+    'host': process.env.AWS_DB_HOST,
+    'dialect': process.env.AWS_DB_DIALECT,
+    'aws_region': process.env.AWS_S3_REGION,
+    'aws_profile': process.env.AWS_S3_CREDENTIAL_PROFILE,
+    'aws_media_bucket': process.env.AWS_S3_BUCKET
+  },
+  'prod': {
+    'username': '',
+    'password': '',
+    'database': 'udagram_prod',
+    'host': '',
+    'dialect': 'postgres'
+  },
+  'jwt': {
+    'secret': process.env.UDAGRAM_JWT_SECRET
+  }
+
+};
